32d15a8d36e85b8f5c2f8b96131d5ca8ca0b3be4
adilkhurshedAlgo <adil.khurshed@algo.com>
2023-08-13 20:18:15 +0500

    Notification Hub added

---
+++ RealTimeChatAndNotifications.Backend/Notifications/NotificationsHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace RealTimeChatAndNotifications.Backend.Notifications
+{
+    public class NotificationsHub : Hub
+    {
+        public NotificationsHub() { }
+
+        public async Task BroadCastNotificaiton(string notification)
+        {
+            await Clients.All.SendAsync("NotificationReceived", notification);
+        }
+    }
+}
