af826ccc40138748f8cdae2be5a15c4210db7c1d
adilkhurshedAlgo <adil.khurshed@algo.com>
2023-08-14 21:20:35 +0500

    Console App SignalR Client listner

---
+++ RealTimeChatAndNotifications.ConsoleClient/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.AspNetCore.SignalR.Client;
+
+try
+{
+    Console.WriteLine("Hello, World!");
+    //Set connection
+    var connection = new HubConnectionBuilder()
+                .WithUrl("wss://localhost:7091/notifications-hub")
+                .Build();
+    //connection.On<string>("BroadCastNotificaiton", (user, message) =>
+    //{
+    //    Console.WriteLine(message);
+    //});
+    await connection.StartAsync();
+
+    Console.WriteLine("Press any key to exit");
+
+    connection.On<string>("BroadCastNotificaiton", (message) =>
+    {
+        Console.WriteLine(message);
+    });
+    Console.ReadKey();
+}
+catch (Exception)
+{
+    Console.WriteLine();
+}
